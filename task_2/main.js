e2eed41dacce3e292d0d742eabf7c728d4c30de6
Teslik0 <z12729476@gmail.com>
2024-01-29 11:32:39 +0300

    Создали файл и подключили JS

---
+++ task_2/main.js
@@ -0,0 +1,9 @@
+let temp = 31;
+
+if (temp > 30) {
+    console.log ('Наденьте лёгкую одежду');
+} else if  (temp > 20) {
+    console.log ("Наденьте тёплую одежду");
+} else if (temp < 10) {
+    console.log ("Сегодня холодно. Наденьте зимнюю одежду")
+}
